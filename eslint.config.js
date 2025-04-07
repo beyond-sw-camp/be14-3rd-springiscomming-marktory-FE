b5e81d7ae2b395c2154bfba503da7b2b3bcd0dcb
Morris235 <dltkdah777@gmail.com>
2025-04-07 16:37:23 +0900

    chore : prettier 연동

--- eslint.config.js
+++ eslint.config.js
@@ -2,11 +2,25 @@ import { defineConfig } from "eslint/config";
 import globals from "globals";
 import js from "@eslint/js";
 import pluginVue from "eslint-plugin-vue";
-
+import eslintPluginPrettier from "eslint-plugin-prettier";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,vue}"] },
-  { files: ["**/*.{js,mjs,cjs,vue}"], languageOptions: { globals: globals.browser } },
-  { files: ["**/*.{js,mjs,cjs,vue}"], plugins: { js }, extends: ["js/recommended"] },
-  pluginVue.configs["flat/essential"],
+  {
+    files: ["**/*.{js,mjs,cjs,vue}"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: globals.browser,
+    },
+    plugins: {
+      js,
+      vue: pluginVue,
+      prettier: eslintPluginPrettier,
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...pluginVue.configs["flat/essential"].rules,
+      "prettier/prettier": "warn",
+    },
+  },
 ]);
