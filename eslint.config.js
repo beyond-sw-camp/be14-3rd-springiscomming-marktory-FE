ada57e68c02cc80276cd95d46e4522f531a826d5
Morris235 <dltkdah777@gmail.com>
2025-04-07 16:55:19 +0900

    chore : eslint config 수정

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,25 @@
-import { defineConfig } from "eslint/config";
-import globals from "globals";
-import js from "@eslint/js";
-import pluginVue from "eslint-plugin-vue";
-import eslintPluginPrettier from "eslint-plugin-prettier";
+import { defineConfig } from 'eslint/config'
+import vueParser from 'vue-eslint-parser'
+import globals from 'globals'
+import js from '@eslint/js'
+import pluginVue from 'eslint-plugin-vue'
+import eslintPluginPrettier from 'eslint-plugin-prettier'
+import babelParser from '@babel/eslint-parser'
 
 export default defineConfig([
   {
-    files: ["**/*.{js,mjs,cjs,vue}"],
+    files: ['**/*.{js,mjs,cjs,vue}'],
+    ignores: ['node_modules', 'dist', 'build', '.output', 'coverage', '.vercel', '.netlify'],
     languageOptions: {
-      ecmaVersion: "latest",
-      sourceType: "module",
+      ecmaVersion: 'latest',
+      sourceType: 'module',
       globals: globals.browser,
+      parser: vueParser,
+      parserOptions: {
+        ecmaFeatures: { jsx: true },
+        requireConfigFile: false,
+        parser: babelParser,
+      },
     },
     plugins: {
       js,
@@ -19,8 +28,8 @@ export default defineConfig([
     },
     rules: {
       ...js.configs.recommended.rules,
-      ...pluginVue.configs["flat/essential"].rules,
-      "prettier/prettier": "warn",
+      ...pluginVue.configs['flat/essential'].rules,
+      'prettier/prettier': 'warn',
     },
   },
-]);
+])
