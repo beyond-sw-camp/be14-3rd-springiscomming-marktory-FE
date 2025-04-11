dc10022fe642dba2764b4862b9a3a840e099be25
Morris235 <dltkdah777@gmail.com>
2025-04-11 11:00:12 +0900

    feat : 전역 테마 색상 적용

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,31 @@
 </template>
 
 <script setup>
-  import { RouterLink, RouterView } from "vue-router";
+import { RouterLink, RouterView } from "vue-router";
 </script>
-<style scoped></style>
+<style>
+/* 공통 다크 테마 */
+.theme-dark {
+  background-color: #000;
+  color: #fff;
+  min-height: 100vh;
+  font-family: 'Helvetica Neue', sans-serif;
+}
+
+/* 링크 등은 기본으로 흰색 */
+a {
+  color: #fff;
+  text-decoration: none;
+}
+
+a:hover {
+  text-decoration: underline;
+}
+
+/* Bootstrap 오버라이드 */
+body {
+  background-color: #000;
+  color: #fff;
+  margin: 0;
+}
+</style>
