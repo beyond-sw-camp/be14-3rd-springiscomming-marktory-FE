91c9fff410af04c2268cde965e70fa260c912b99
RKDLDE <kangcanddy@gmail.com>
2025-04-11 14:16:36 +0900

    chore: 폰트추가

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,17 @@
     <nav>
       <RouterLink to="/" />
     </nav>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR&display=swap" rel="stylesheet" />
+
   </header>
   <main>
     <RouterView />
   </main>
+  
 </template>
 
 <script setup>
   import { RouterLink, RouterView } from "vue-router";
 </script>
-<style scoped></style>
+<style scoped>
+</style>
