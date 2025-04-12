cff48b1c76a5fe9cf084a40bc53e51fee8e5057d
LAPTOP-4PKBB9GJ\Playdata <tommy8969@naver.com>
2025-04-12 17:57:32 +0900

    feat: 마이페이지 프로필 json 서버 연동

--- src/components/mypage/SideProfile.vue
+++ src/components/mypage/SideProfile.vue
@@ -1,63 +1,75 @@
 <template>
-    <div class="profile-card">
-      <img class="profile-img" :src="profileImg" alt="프로필 이미지"/>
-      <div class="nickname">{{ nickname }}</div>
-      <div class="email">{{ email }}</div>
-      <div class="follow-info">
-        <img src="../icons/follow-icon.svg"/>      
-        <span><strong>{{ followers }}</strong> followers · <strong>{{ following }}</strong> following</span>
-      </div>
+  <div class="profile-card">
+    <img class="profile-img" :src="member.image" alt="프로필 이미지"/>
+    <div class="nickname">{{ member.nickname }}</div>
+    <div class="email">{{ member.email }}</div>
+    <div class="follow-info">
+      <img src="../../assets/icons/follow-icon.svg"/>      
+      <span><strong>{{ member.followerCount }}</strong> followers · <strong>{{ member.followingCount }}</strong> following</span>
     </div>
-  </template>
+  </div>
+</template>
     
-  <script setup>
-    import imgage from './D_profile_IMG.png';
+<script setup>
+  import { ref, onMounted } from 'vue';
   
-    const profileImg = imgage;
-    const nickname = 'D';
-    const email = 'D1112@mail.com';
-    const followers = 122;
-    const following = 32;
-  </script>
-    
-  <style scoped>
-    .profile-card {
-      width: 262px;
-      height: 361px;
-      margin-top: 404px;
-      padding-top: 19px;
-      background-color: black;
-      position: fixed;
-      text-align: center;
+  const member = ref({});
+
+  onMounted(async () => {
+    try {
+      const res = await fetch('http://localhost:3001/members/11');
+
+      if (!res.ok) {
+        throw new Error('서버 응답 실패');
+      }
+
+      const data = await res.json();
+      member.value = data;
+    } catch (err) {
+      console.error('Fetch 실패:', err);
     }
+  });
+
+</script>
   
-    .profile-img {
-      width: 196px;
-      height: 196px;
-      margin: 0 auto;
-      border-radius: 50%;
-      object-fit: cover;
-    }
+<style scoped>
+  .profile-card {
+    width: 262px;
+    height: 361px;
+    margin-top: 404px;
+    padding-top: 19px;
+    background-color: black;
+    position: fixed;
+    text-align: center;
+  }
+
+  .profile-img {
+    width: 196px;
+    height: 196px;
+    margin: 0 auto;
+    border-radius: 50%;
+    object-fit: cover;
+  }
+
+  .nickname {
+    margin-top: 10px;
+    padding: 5px 56px;
+    font-size: 32px;
+    display: inline-block;
+    color: white;
+    border-bottom: 2px solid #FD6F22;
+  }
+
+  .email {
+    padding-top: 10px;
+    padding-bottom: 17px;
+    font-size: 16px;
+    color: white;
+  }
+
+  .follow-info {
+    font-size: 14px;
+    color: white;
+  }
+</style>
   
-    .nickname {
-      margin-top: 10px;
-      padding: 5px 56px;
-      font-size: 32px;
-      display: inline-block;
-      color: white;
-      border-bottom: 2px solid #FD6F22;
-    }
-  
-    .email {
-      padding-top: 10px;
-      padding-bottom: 17px;
-      font-size: 16px;
-      color: white;
-    }
-  
-    .follow-info {
-      font-size: 14px;
-      color: white;
-    }
-  </style>
-    
