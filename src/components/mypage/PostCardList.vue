e69f60ed769bd227561ac29dda18eea712754d8c
LAPTOP-4PKBB9GJ\Playdata <tommy8969@naver.com>
2025-04-12 15:11:56 +0900

    feat: 게시글, 템플릿 카드 리스트 컴포넌트 초안 구성

---
+++ src/components/mypage/PostCardList.vue
@@ -0,0 +1,117 @@
+<template>
+    <div class="card-list">
+      <PostCard
+        v-for="(post, index) in postList"
+        :key="index"
+        :title="post.title"
+        :date="post.date"
+        :thumbnailImg="post.thumbnailImg"
+        :profileImg="post.profileImg"
+        :nickname="post.nickname"/>
+    </div>
+  </template>
+    
+  <script setup>
+    import PostCard from './PostCard.vue';
+    import imgage from '../icons/D_profile_IMG.png';
+    
+    const postList = [
+      {
+        title: '파이썬 기초 문법 정리',
+        date: '2024-03-01',
+        thumbnailImg: imgage,
+        profileImg: imgage,
+        nickname: 'Dong'
+      },
+      {
+        title: '여행 준비 체크리스트 ✈️',
+        date: '2024-03-02',
+        thumbnailImg: imgage,
+        profileImg: imgage,
+        nickname: 'Jisoo'
+      },
+      {
+        title: '헬스 가이드 💪',
+        date: '2024-03-03',
+        thumbnailImg: imgage,
+        profileImg: imgage,
+        nickname: 'Ironman'
+      },
+      {
+        title: '파이썬 기초 문법 정리',
+        date: '2024-03-01',
+        thumbnailImg: imgage,
+        profileImg: imgage,
+        nickname: 'Dong'
+      },
+      {
+        title: '여행 준비 체크리스트 ✈️',
+        date: '2024-03-02',
+        thumbnailImg: imgage,
+        profileImg: imgage,
+        nickname: 'Jisoo'
+      },
+      {
+        title: '헬스 가이드 💪',
+        date: '2024-03-03',
+        thumbnailImg: imgage,
+        profileImg: imgage,
+        nickname: 'Ironman'
+      },
+      {
+        title: '파이썬 기초 문법 정리',
+        date: '2024-03-01',
+        thumbnailImg: imgage,
+        profileImg: imgage,
+        nickname: 'Dong'
+      },
+      {
+        title: '여행 준비 체크리스트 ✈️',
+        date: '2024-03-02',
+        thumbnailImg: imgage,
+        profileImg: imgage,
+        nickname: 'Jisoo'
+      },
+      {
+        title: '헬스 가이드 💪',
+        date: '2024-03-03',
+        thumbnailImg: imgage,
+        profileImg: imgage,
+        nickname: 'Ironman'
+      },
+      {
+        title: '여행 준비 체크리스트 ✈️',
+        date: '2024-03-02',
+        thumbnailImg: imgage,
+        profileImg: imgage,
+        nickname: 'Jisoo'
+      },
+      {
+        title: '헬스 가이드 💪',
+        date: '2024-03-03',
+        thumbnailImg: imgage,
+        profileImg: imgage,
+        nickname: 'Ironman'
+      }
+    ];
+  </script>
+    
+  <style scoped>
+    .card-list {
+      width: 1045px;
+      margin-top: 160px;
+      margin-left: 16px;
+      padding-right: 8px;  
+      display: grid;
+      position: relative;
+      gap: 24px;
+      overflow-y: auto;     
+      grid-template-columns: repeat(3, 1fr);
+    }
+  
+    .card-list::-webkit-scrollbar {
+      display: none;                
+    }
+  
+  </style>
+    
